1fb2f172f03ab02987b7eb8fc6f77ce373d938a7
Ignacio Kingston <kingston.ignacio2@gmail.com>
2023-09-27 00:37:58 +0200

    installation package sharp  + fonctions ratingBook et getBestBooks (encore à travailler les erreurs) + routage

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -4,26 +4,42 @@ const Book = require ('../models/book');
 //import package fs - file system- pour gérer modifications (supprimer) des fichiers
 const fs = require('fs');
 
-//logique route POST
-exports.createBook = (req, res, next)=> {
+//import package sharp - por optimiser les images
+const sharp = require ('sharp');
+
+//fonction POST - créer un livre
+exports.createBook = async (req, res, next)=> {
     //transformation d'un objet string à un objet JS exploitable
     const bookObject = JSON.parse(req.body.book);
     //par messure de sécurité; on les remplacent par le userId du token par middleware de auth
     delete bookObject._id;
     delete bookObject._userId;
+
     const book= new Book({
         ...bookObject,
         userId: req.auth.userId,
         //pour résoudre l'URL complète de l'image (ATT méthode GET !)
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
+
+    console.log('Before Sharp:', req.file.filename);
+
+
+        console.log('After Sharp:', req.file.filename);
+
         //méthode save pour enregistrer dans la base de données
         book.save()
-        .then (() => {res.status(201).json ({message: 'Livre enregistré !'})})
-        .catch(error => {res.status(400).json({ error })})
-};
+        .then (() => {
+                    res.status(201).json({ message: 'Livre enregistré !' });
+            })
+        .catch(error => {
+            res.status(400).json({ error });
+        }); 
+    };  
 
-//logique route GET x 1 élément en particulier
+
+
+//fonction GET x 1 élément en particulier
 exports.getOneBook = (req, res, next) =>{
     Book.findOne({_id: req.params.id})
     .then((book)=> {res.status(200).json(book);
@@ -32,7 +48,7 @@ exports.getOneBook = (req, res, next) =>{
     });
 };
 
-//logique route PUT x modifier un élément
+//fonction PUT x modifier un élément
 exports.modifyBook = (req, res, next) =>{
     //pour vérifier s'il y a un fichier
     const bookObject= req.file ? {
@@ -59,7 +75,7 @@ exports.modifyBook = (req, res, next) =>{
         });
 };
 
-//logique route DELETE x supprimer un élément
+//fonction DELETE x supprimer un élément
 exports.deleteBook = (req, res, next) =>{
     //pour vérifier le userId
     Book.findOne({ _id: req.params.id})
@@ -82,12 +98,73 @@ exports.deleteBook = (req, res, next) =>{
     });
 };
 
-//logique route GET x montrer les livres
-exports.getAllBook = (req, res, next) =>{
+//fonction GET x montrer les livres
+exports.getAllBooks = (req, res, next) =>{
     //méthode find pour récuperer les livres
     Book.find()
     .then((books) => {res.status(200).json(books);
     })
     .catch((error) => {res.status(400).json({ error });
     });
+};
+
+//fonction GET x montrer les meilleurs livres
+exports.getBestBooks = async (req, res, next) => {
+    try {
+        const bestBooks = await Book.find({})
+            .sort({ averageRating: -1 }) //pour ranger en ordre descendant
+            .limit(3); //limitation max de 3 livres
+
+        res.status(200).json(bestBooks);
+    } catch (error) {
+        res.status(500).json({ error: "Erreur au moment de montrer les meilleurs livres"})
+    }
+};
+
+//fonction POST - rating
+exports.ratingBook = (req, res, next) => {
+    //Extraction des données de la rêquete
+    const userId = req.auth.userId;
+    const userRating = req.body.rating;
+    //cherche le book par son id
+    Book.findByIdAndUpdate({_id: req.params.id})
+    .then(book => {
+
+        console.log('ID du livre dans req.params:', req.params.id);
+
+        if(book.ratings.some((r) => r.userId === userId)) {
+            return res.status(400).json({ error: 'Vous avez déjà qualifié ce livre'});
+        }
+
+        console.log('Livre avant de sauvegarder:', book);
+
+        //Ajout la note du livre
+        book.ratings.push({ userId, userRating});
+        //Mise à jour de la note moyenne
+        book.averageRating = calculateAverageRating(book.ratings);
+
+        console.log('Livre après mise à jour de la note moyenne:', book);
+
+        //Sauvegarde de changements
+        return book.save()
+        .then(book => {res.json(book)})
+        .then(()=> {
+        res.status(201).json({ message: 'Note ajoutée correctement !', book});
+        }) 
+    })
+
+    .catch ((error) => {
+    res.status(500).json({ error: 'Erreur au moment d\'ajouter la note !'});
+    });
+};
+
+
+//fonction pour calculer la note moyenne
+function calculateAverageRating(ratings) {
+    //pour verifier s'il n y a pas de notes
+    if(ratings.length === 0) return 0;
+    //utilisation du méthode reduce pour faire une somme de toutes les notes
+    const sum = ratings.reduce((total, rating) => total + rating.rating, 0);
+    //pour calculer la moyenne - la somme divisée par la quantité de notes faites
+    return sum / ratings.length;
 };
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -8,13 +8,16 @@ const auth = require('../middleware/auth');
 //import middleware multer-config
 const multer = require('../middleware/multer-config');
 
+//import controllers book
 const bookCtrl = require('../controllers/book');
 
 //routage grâce au méthode router
-router.get('/', bookCtrl.getAllBook); //tous les livres (pas besoin de auth)
+router.get('/', bookCtrl.getAllBooks); //tous les livres (pas besoin de auth)
 router.post('/', auth, multer, bookCtrl.createBook); //ajout multer après auth par sécurité
 router.get('/:id', bookCtrl.getOneBook); //un livre (pas besoin de auth)
-router.put('/:id', auth, multer, bookCtrl.modifyBook);
-router.delete('/:id', auth, bookCtrl.deleteBook);
+router.put('/:id', auth, multer, bookCtrl.modifyBook); // modifier un livre
+router.delete('/:id', auth, bookCtrl.deleteBook); // supprimer un livre
+router.get('/bestrating', bookCtrl.getBestBooks); // meilleurs livres
+router.post('/:id/rating', auth, bookCtrl.ratingBook); // notes des livres
 
 module.exports = router;
